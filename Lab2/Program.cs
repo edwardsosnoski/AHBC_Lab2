50bee8cbf9e4c200fb0dd166f218795425d700fa
edwardsosnoski <edward.sosnoski@gmail.com>
2019-07-11 21:34:01 -0400

    created program with added flavor: will only continue if user agrees

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -6,8 +6,34 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //This is a test of my ability to commit.
+            bool continuePrompt = false;
+            do
+            {
+                Console.WriteLine("Welcome to Grand Circus's Room Detail Generator!");
+                Console.WriteLine("Enter Length:");
+                string userRoomLength = Console.ReadLine();
+                Console.WriteLine("Enter Width:");
+                string userRoomWidth = Console.ReadLine();
+
+                double roomArea = (double.Parse(userRoomLength) * double.Parse(userRoomWidth));
+                double roomPerimeter = ((double.Parse(userRoomLength) * 2) + (double.Parse(userRoomWidth) * 2));
+
+                Console.WriteLine($"Area: {roomArea}");
+                Console.WriteLine($"Perimeter: {roomPerimeter}");
+
+                Console.WriteLine("Continue? (y/n)");
+                string userContinuePrompt = Console.ReadLine();
+
+                if (userContinuePrompt == "y")
+                {
+                    continuePrompt = true;
+                }
+                else
+                {
+                    continuePrompt = false;
+                }
+            }
+            while (continuePrompt);
         }
     }
 }
